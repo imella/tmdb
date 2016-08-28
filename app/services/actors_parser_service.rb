4d3dc1b47aa80372a3dc3fa684c230acc1488ba0
Ignacio Mella <imellat@gmail.com>
2016-08-28 18:56:17 -0300

    Refactor actors parser

--- app/services/actors_parser_service.rb
+++ app/services/actors_parser_service.rb
@@ -2,27 +2,12 @@ require 'csv'
 
 # Reads all .data files from given directory
 # and saves each actor in the database
-class ActorsParserService
-  def self.call(*args)
-    new(*args).call
-  end
-
-  def initialize(path:)
-    @column_separator = '|'
-    @files = Dir.glob("#{path}/*.data")
-  end
-
-  def call
-    @files.each do |filename|
-      save_actor(filename)
-    end
-  end
+class ActorsParserService < ParserService
 
   private
 
-  def save_actor(filename)
-    actor_data = read_data(filename)
-    Actor.create(actor_data)
+  def save(filename)
+    Actor.create(read_data(filename))
   end
 
   def read_data(filename)
@@ -33,6 +18,7 @@ class ActorsParserService
       parsed_data[:name] = row[1]
       parsed_data[:birthdate] = Time.at(row[2].to_i / 1_000)
       parsed_data[:bio] = row[3]
+      parsed_data[:image_path] = "actors/#{row[0]}.jpg"
     end
     parsed_data
   end
