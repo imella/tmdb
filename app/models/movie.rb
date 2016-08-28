dd5bfe250ee0da5183cadb1ce55f480939affe5e
Ignacio Mella <imellat@gmail.com>
2016-08-28 17:11:48 -0300

    Validate uniquness of slug for both movies and actors

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,12 @@
 # Movie Model Class
 # Columns
+# id          - integer
 # name        - string
 # slug        - string
 # year        - integer
 # description - text
 # image_path  - string
 class Movie < ApplicationRecord
-  validates :name, :slug, :year, :description
+  validates :name, :slug, :year, :description, presence: true
+  validates :slug, uniqueness: true
 end
