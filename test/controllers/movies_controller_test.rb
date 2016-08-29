70ddc228f9a6cf4f01072eec716f5b313a78109a
Ignacio Mella <imellat@gmail.com>
2016-08-28 21:23:53 -0300

    Add movies controller test

---
+++ test/controllers/movies_controller_test.rb
@@ -0,0 +1,12 @@
+class MoviesControllerTest < ActionDispatch::IntegrationTest
+  test 'should get index' do
+    get movies_url
+    assert_response :success
+  end
+
+  test 'should get show' do
+    movie = movies(:one)
+    get movie_url(movie)
+    assert_response :success
+  end
+end
